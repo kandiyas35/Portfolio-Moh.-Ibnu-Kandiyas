fe52e115385d72298a105fbe83daf5ee7a6d1781
Moh. Ibnu Kandiyas <150608091+kandiyas35@users.noreply.github.com>
2025-07-16 13:53:49 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,19 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // ✅ Tambahkan base untuk path GitHub Pages
+  base: "/Portfolio-Moh.-Ibnu-Kandiyas/",
+
   server: {
     host: "::",
     port: 8080,
   },
+
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
